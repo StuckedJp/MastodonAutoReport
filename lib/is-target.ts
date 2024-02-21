af21e2d89502cc043426b4a210d0f8785c684af3
Sakai Takao <s.takao.73+github@gmail.com>
2024-02-21 20:14:40 +0900

    リファクタ

---
+++ lib/is-target.ts
@@ -0,0 +1,24 @@
+import masto from "masto";
+const config = require("../env.json");
+
+export type IsTargetParam = {
+  status: masto.mastodon.v1.Status;
+};
+
+export const isTarget = (param: IsTargetParam): boolean => {
+  const retVal =
+    !!param.status.mentions.find((m: any) => m.acct === config.acctMe) &&
+    param.status.mentions.length >= config.mentionCount &&
+    param.status.account.followersCount <= config.minFollowers;
+
+  console.log(
+    new Date().toISOString(),
+    param.status.id,
+    param.status.mentions.find((m: any) => m.acct === config.acctMe),
+    param.status.mentions.length >= config.mentionCount,
+    param.status.account.followersCount <= config.minFollowers,
+    retVal
+  );
+
+  return retVal;
+};
---
+++ lib/report-and-block.ts
@@ -0,0 +1,29 @@
+import masto from "masto";
+
+export type ReportAndBlockParam = {
+  status: masto.mastodon.v1.Status;
+};
+
+export const reportAndBlock = async (
+  rest: masto.mastodon.rest.Client,
+  param: ReportAndBlockParam
+) => {
+  const report = await rest.v1.reports.create({
+    accountId: param.status.account.id,
+    category: "spam",
+    forward: true,
+    statusIds: [param.status.id],
+    comment: "Automatic report.",
+  });
+  await rest.v1.admin.accounts.$select(param.status.account.id).action.create({
+    type: "suspend",
+    reportId: report.id,
+  });
+  await rest.v1.accounts.$select(param.status.account.id).block();
+  console.log(
+    `#############################################################################`
+  );
+  console.log(
+    `Reported: from:${param.status.account.acct} statusId:${param.status.id} URI:${param.status.uri} content:${param.status.content}`
+  );
+};
