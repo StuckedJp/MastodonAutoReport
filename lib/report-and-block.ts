022beac8e00f8f508dc9c9da6f87337d36a95734
Sakai Takao <s.takao.73+github@gmail.com>
2024-02-22 15:53:39 +0900

    エラー処理を追加。

--- lib/report-and-block.ts
+++ lib/report-and-block.ts
@@ -8,22 +8,25 @@ export const reportAndBlock = async (
   rest: masto.mastodon.rest.Client,
   param: ReportAndBlockParam
 ) => {
-  const report = await rest.v1.reports.create({
-    accountId: param.status.account.id,
-    category: "spam",
-    forward: true,
-    statusIds: [param.status.id],
-    comment: "Automatic report.",
-  });
-  await rest.v1.admin.accounts.$select(param.status.account.id).action.create({
-    type: "suspend",
-    reportId: report.id,
-  });
-  await rest.v1.accounts.$select(param.status.account.id).block();
-  console.log(
-    `#############################################################################`
-  );
-  console.log(
-    `Reported: from:${param.status.account.acct} statusId:${param.status.id} URI:${param.status.uri} content:${param.status.content}`
-  );
+  try {
+    const report = await rest.v1.reports.create({
+      accountId: param.status.account.id,
+      category: "spam",
+      forward: true,
+      statusIds: [param.status.id],
+      comment: "Automatic report.",
+    });
+    await rest.v1.admin.accounts
+      .$select(param.status.account.id)
+      .action.create({
+        type: "suspend",
+        reportId: report.id,
+      });
+    await rest.v1.accounts.$select(param.status.account.id).block();
+    console.log(
+      `Reported: from:${param.status.account.acct} statusId:${param.status.id} URI:${param.status.uri} content:${param.status.content}`
+    );
+  } catch (err) {
+    console.error(err);
+  }
 };
